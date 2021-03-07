55209f35f342f2c4e76714095a6d95bae7bdc1d5
Evangelos Athanasiadis <dyrer@athanasiadis.me>
2021-03-08 00:56:00 +0200

    Init

--- social-links.php
+++ social-links.php
@@ -1,1 +1,20 @@
 <?php
+
+/**
+ * Plugin Name: Social Links
+ * Description: Adds social icons with links to profiles
+ * Version: 1.0
+ * Author: Evangelos Athanasiadis
+ */
+
+
+// Exit if Accesses directly
+if(!defined('ABSPATH')) {
+	exit;
+}
+
+// Load scripts
+require_once(plugin_dir_path(__FILE__). '/includes/social-links-scripts.php');
+
+// Load class
+require_once(plugin_dir_path(__FILE__). '/includes/social-links-class.php');
